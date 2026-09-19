4ad376b46ecc956d9667bbc42543a9ef9550698e
G.Ameku <genta.ameku.work@gmail.com>
2026-09-19 12:53:55 +0900

    chore: update copyright year to 2026

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="w-full">
       <div className="container mx-auto flex h-[var(--custom-footer-height)] items-center justify-center px-4 md:px-6">
-        <p>Copyright &copy; 2025 Genta Ameku. All Rights Reserved.</p>
+        <p>Copyright &copy; 2026 Genta Ameku. All Rights Reserved.</p>
       </div>
     </footer>
   );
